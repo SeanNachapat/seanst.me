90cac20ef9b40bcf77998a2f73d05184fb1ed32f
Nachapat Iamphuang <61649121+SeanNachapat@users.noreply.github.com>
2025-02-10 21:57:04 +0700

    commit 💬

---
+++ src/components/home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="home">
+      <h1 className="heading">Hey there! I'm Sean 👋</h1>
+      <p className="description">:3</p>
+    </div>
+  );
+}
+
+export default Home;
