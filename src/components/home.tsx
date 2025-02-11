5a7ec0f0e5eea3eb28623b36e462e9e6cce181e3
Nachapat Iamphuang <61649121+SeanNachapat@users.noreply.github.com>
2025-02-11 21:45:03 +0700

    commit 💬

--- src/components/home.tsx
+++ src/components/home.tsx
@@ -2,7 +2,17 @@ function Home() {
   return (
     <div className="home">
       <h1 className="heading">Hey there! I'm Sean 👋</h1>
-      <p className="description">:3</p>
+      <p className="description">💻 Studying Computer Science at KMITL</p>
+      <div>
+        <a href="https://github.com/SeanNachapat" className="link">
+          {" "}
+          Github
+        </a>
+        <a href="https://www.instagram.com/seanst._/" className="link">
+          {" "}
+          Instagram
+        </a>
+      </div>
     </div>
   );
 }
